2c242394c74baef1bf3dbb11f35eb6577766ad80
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2025-02-17 19:47:14 -0800

    The HTTP server that can be terminated (`/quit`, Ctrl+C, `docker stop`, all tested).

---
+++ step02_server/code/src/main.rs
@@ -0,0 +1,44 @@
+use axum::{routing::get, Router, serve};
+use std::net::SocketAddr;
+use tokio::{net::TcpListener, sync::mpsc};
+use tokio::signal::unix::{signal, SignalKind};
+
+#[tokio::main]
+async fn main() {
+  let (shutdown_tx, mut shutdown_rx) = mpsc::channel::<()>(1);
+
+  let app = Router::new()
+    .route("/healthz", get(|| async { "OK\n" }))
+    .route("/", get(|| async { "hello this is a rust http server\n" }))
+    .route(
+        "/quit",
+        get({
+          let shutdown_tx = shutdown_tx.clone();
+          move || {
+            let shutdown_tx = shutdown_tx.clone();
+            async move {
+              let _ = shutdown_tx.send(()).await;
+              "yes i am shutting down\n"
+            }
+          }
+        }));
+
+  let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+  let listener = TcpListener::bind(addr).await.unwrap();
+
+  println!("rust http server ready on {}", addr);
+
+  let server = serve(listener, app);
+
+  let mut term_signal = signal(SignalKind::terminate()).expect("failed to register SIGTERM handler");
+  let mut int_signal = signal(SignalKind::interrupt()).expect("failed to register SIGINT handler");
+
+  tokio::select! {
+    _ = server.with_graceful_shutdown(async move { shutdown_rx.recv().await; }) => { println! ("done"); }
+    _ = tokio::signal::ctrl_c() => { println!("terminating due to Ctrl+C"); }
+    _ = term_signal.recv() => { println!("terminating due to SIGTERM"); }
+    _ = int_signal.recv() => { println!("terminating due to SIGINT"); }
+  }
+
+  println!("rust http server down");
+}
---
+++ step02_server/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+docker build . -t demo
+
+docker run --rm --network=bridge -p 3000:3000 -t demo &
+PID=$!
+
+while true ; do
+  R="$(curl -s localhost:3000/healthz || echo NOPE)"
+  if [ "$R" = "OK" ] ; then
+    echo "server healthy"
+    break
+  fi
+  sleep 0.5
+  echo "server not yet healthy"
+done
+
+curl -s localhost:3000
+
+curl -s localhost:3000/quit
+
+wait $PID
