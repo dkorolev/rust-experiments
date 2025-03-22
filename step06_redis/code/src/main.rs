f89d6bca158428de43ab25134fcba2a9311d9469
Maxim Losev <losevmv@gmail.com>
2025-03-22 23:45:44 +0300

    Rustifying redis example

--- step06_redis/code/src/main.rs
+++ step06_redis/code/src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
 use redis::AsyncCommands;
-use std::future::Future;
 use std::process::ExitCode;
+use anyhow::Result;
+use anyhow::anyhow;
 
 #[derive(Parser)]
 struct Args {
@@ -12,48 +13,34 @@ struct Args {
   mode: String,
 }
 
-async fn run_check(args: &Args) -> Result<u8, redis::RedisError> {
+async fn try_check(args: &Args) -> Result<()> {
   let _ = redis::Client::open(args.redis.to_string())?.get_multiplexed_async_connection().await?;
   println!("Redis is available.");
-  Ok(0)
+  Ok(())
 }
 
-async fn run_test(args: &Args) -> Result<u8, redis::RedisError> {
+async fn try_test(args: &Args) -> Result<()> {
   let client = redis::Client::open(args.redis.to_string())?;
   let mut con = client.get_multiplexed_async_connection().await?;
 
-  let _: () = con.set("key", "value").await.expect("failed");
+  let _: () = con.set("key", "value").await?;
   let val: String = con.get("key").await?;
 
   println!("Got value: {}", val);
 
-  if val == "value" { Ok(0) } else { Ok(1) }
-}
-
-async fn run_arm<'a, F, RETVAL>(f: F, args: &'a Args) -> ExitCode
-where
-  F: FnOnce(&'a Args) -> RETVAL,
-  RETVAL: Future<Output = Result<u8, redis::RedisError>>,
-{
-  let result = f(&args).await;
-  ExitCode::from(match result {
-    Ok(code) => code,
-    _ => {
-      println!("Something failed, or Redis is not available.");
-      1
-    }
-  })
+  (val == "value").then(|| ()).ok_or(anyhow!("Wrong value"))
 }
 
 #[tokio::main]
 async fn main() -> ExitCode {
   let args = Args::parse();
+
   match args.mode.as_str() {
-    "check" => run_arm(&run_check, &args).await,
-    "test" => run_arm(&run_test, &args).await,
+    "check" => try_check(&args).await.map_or(1, |_| 0),
+    "test" => try_test(&args).await.map_or(1, |_| 0),
     _ => {
       println!("This `--mode` value is not supported.");
-      ExitCode::from(1)
+      1
     }
-  }
+  }.into()
 }
