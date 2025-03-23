3b97ee048647ca921a76d101a85db397dd26e411
Dima Korolev <dima@current.ci>
2025-03-23 13:11:30 -0700

    Several runs to confirm the DB is indeed persisted.

--- step07_redb/run.sh
+++ step07_redb/run.sh
@@ -5,25 +5,32 @@ set -e
 docker build -f ../Dockerfile.template . -t demo
 
 mkdir -p .db
-docker run --rm -v ./.db/:/.db/ --network=bridge -p 3000:3000 -t demo &
-PID=$!
 
-while true ; do
-  R="$(curl -s localhost:3000/healthz || echo NOPE)"
-  if [ "$R" = "OK" ] ; then
-    echo "server healthy"
-    break
-  fi
-  sleep 0.5
-  echo "server not yet healthy"
+N=3
+for i in $(seq 1 $N) ; do
+  echo "Run $i of $N."
+
+  docker run --rm -v ./.db/:/.db/ --network=bridge -p 3000:3000 -t demo &
+  PID=$!
+
+  while true ; do
+    R="$(curl -s localhost:3000/healthz || echo NOPE)"
+    if [ "$R" = "OK" ] ; then
+      echo "server healthy"
+      break
+    fi
+    sleep 0.5
+    echo "server not yet healthy"
+  done
+
+  curl -s localhost:3000
+
+  curl -s localhost:3000/json
+  echo
+
+  curl -s localhost:3000/quit
+  wait $PID
+  echo
 done
 
-curl -s localhost:3000
-
-curl -s localhost:3000/json
-echo
-
-curl -s localhost:3000/quit
-echo
-
-wait $PID
+echo "All runs done."
