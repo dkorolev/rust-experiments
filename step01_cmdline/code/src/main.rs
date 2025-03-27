d92ac77f82d490329fe24bb54194f451d82c39e9
Dima Korolev <dima@current.ci>
2025-03-27 12:36:20 -0700

    A quick demo of using Windsurf on a stream.

--- step01_cmdline/code/src/main.rs
+++ step01_cmdline/code/src/main.rs
@@ -1,14 +1,26 @@
 use clap::Parser;
 
+#[derive(Parser, clap::ValueEnum, Clone, Debug)]
+enum Operation {
+  Add,
+  Mul,
+}
+
 #[derive(Parser)]
 struct Args {
   #[arg(long)]
   a: i8,
   #[arg(long)]
   b: i8,
+  #[arg(long, value_enum, default_value_t = Operation::Add)]
+  op: Operation,
 }
 
 fn main() {
   let args = Args::parse();
-  println!("{} + {} = {}", args.a, args.b, args.a + args.b);
+  let (result, op_symbol) = match args.op {
+    Operation::Add => (args.a + args.b, '+'),
+    Operation::Mul => (args.a * args.b, '*'),
+  };
+  println!("{} {} {} = {}", args.a, op_symbol, args.b, result);
 }
