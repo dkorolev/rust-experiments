a90594d692aa3ddd090abc0b32e2ac75854b485e
Dima Korolev <dima@current.ci>
2025-04-02 21:11:22 -0700

    More Rusty.

--- step07_redb/code/src/db.rs
+++ step07_redb/code/src/db.rs
@@ -1,5 +1,6 @@
 use redb::{Database, ReadableTable, TableDefinition};
 use std::error::Error;
+use std::sync::Arc;
 use tokio::sync::{mpsc, oneshot};
 
 // NOTE(dkorolev): This is a hack to avoid having to deal with the error type.
@@ -11,13 +12,37 @@ static GLOBALS: TableDefinition<u64, u64> = TableDefinition::new("globals");
 
 const DEFAULT_REQUEST_QUEUE_SIZE: usize = 32;
 
-#[derive(Debug)]
-enum DbRequest {
-  IncCounter { idx: u64, response: DbResultAwaiter<u64> },
+trait DbRequestHandler: Send + Sync {
+  type Response: Send;
+  fn handle(&self, db: &Database) -> DbResult<Self::Response>;
+}
+
+#[derive(Debug, Clone)]
+struct IncCounterRequest {
+  idx: u64,
+}
+
+impl DbRequestHandler for IncCounterRequest {
+  type Response = u64;
+  fn handle(&self, db: &Database) -> DbResult<u64> {
+    let mut counter_value: u64 = 0;
+    let txn = db.begin_write()?;
+    {
+      let mut table = txn.open_table(GLOBALS)?;
+      if let Some(value) = table.get(&self.idx)? {
+        counter_value = value.value();
+      }
+      counter_value += 1;
+      println!("Run counter in the DB at index {}: {}", self.idx, counter_value);
+      table.insert(&self.idx, &counter_value)?;
+    }
+    txn.commit()?;
+    Ok(counter_value)
+  }
 }
 
 #[derive(Clone)]
-pub struct AsyncRedb(mpsc::Sender<DbRequest>);
+pub struct AsyncRedb(mpsc::Sender<(Arc<dyn DbRequestHandler<Response = u64> + Send + Sync>, DbResultAwaiter<u64>)>);
 
 impl AsyncRedb {
   pub fn new(db: Database) -> Self {
@@ -25,41 +50,20 @@ impl AsyncRedb {
   }
 
   pub fn with_queue_size(db: Database, queue_size: usize) -> Self {
-    let (request_tx, mut request_rx) = mpsc::channel(queue_size);
+    let (request_tx, mut request_rx): (mpsc::Sender<(Arc<dyn DbRequestHandler<Response = u64> + Send + Sync>, DbResultAwaiter<u64>)>, _) = mpsc::channel(queue_size);
 
     std::thread::spawn(move || {
-      while let Some(request) = request_rx.blocking_recv() {
-        // TODO(dkorolev): Use the visitor pattern here, or whatever the Rust way to do this is.
-        match request {
-          DbRequest::IncCounter { idx, response } => {
-            let _ = response.send(Self::handle_inc_counter(&db, idx));
-          }
-        }
+      while let Some((request, response)) = request_rx.blocking_recv() {
+        let _ = response.send(request.handle(&db));
       }
     });
 
     Self(request_tx)
   }
 
-  fn handle_inc_counter(db: &Database, idx: u64) -> DbResult<u64> {
-    let mut counter_value: u64 = 0;
-    let txn = db.begin_write()?;
-    {
-      let mut table = txn.open_table(GLOBALS)?;
-      if let Some(value) = table.get(&idx)? {
-        counter_value = value.value();
-      }
-      counter_value += 1;
-      println!("Run counter in the DB at index {idx}: {counter_value}");
-      table.insert(&idx, &counter_value)?;
-    }
-    txn.commit()?;
-    Ok(counter_value)
-  }
-
   pub async fn inc_counter(&self, idx: u64) -> DbResult<u64> {
     let (response_tx, response_rx) = oneshot::channel();
-    self.0.send(DbRequest::IncCounter { idx, response: response_tx }).await?;
+    self.0.send((Arc::new(IncCounterRequest { idx }), response_tx)).await?;
     response_rx.await.map_err(|e| Box::new(e))?
   }
 }
