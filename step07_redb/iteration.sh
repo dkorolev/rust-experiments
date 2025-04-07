64fc7176bb04eae83a793bd9a3f0b7e343f0fc06
Dima Korolev <dima@current.ci>
2025-04-07 22:25:02 +0200

    More tests.

--- step07_redb/iteration.sh
+++ step07_redb/iteration.sh
@@ -24,3 +24,15 @@ curl -s localhost:3000/string
 echo
 curl -s localhost:3000/string
 echo
+
+curl -s http://localhost:3000/journal | jq .
+echo
+
+curl -d '{"id": "test", "a": 2, "b": 2, "c": 5}' http://localhost:3000/sums
+curl -d '{"id": "test", "a": 2, "b": 3, "c": 5}' http://localhost:3000/sums
+
+curl -d '{"id": "test'$(date +%s)'-a", "a": 2, "b": 2, "c": 5}' http://localhost:3000/sums
+curl -d '{"id": "test'$(date +%s)'-b", "a": 2, "b": 3, "c": 5}' http://localhost:3000/sums
+
+curl -s http://localhost:3000/journal | jq .
+echo
