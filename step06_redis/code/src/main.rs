465b2543cf807b7baa306b042c28c2641887addd
Dima Korolev <dima@current.ai>
2025-04-22 07:17:42 -0700

    Redis publish and subscribe.

--- step06_redis/code/src/main.rs
+++ step06_redis/code/src/main.rs
@@ -1,8 +1,10 @@
-use clap::Parser;
-use redis::AsyncCommands;
-use std::process::ExitCode;
 use anyhow::Result;
 use anyhow::anyhow;
+use chrono::Local;
+use clap::Parser;
+use futures::StreamExt;
+use redis::AsyncCommands;
+use std::process::ExitCode;
 
 #[derive(Parser)]
 struct Args {
@@ -31,6 +33,29 @@ async fn try_test(args: &Args) -> Result<()> {
   (val == "value").then(|| ()).ok_or(anyhow!("Wrong value"))
 }
 
+async fn try_pub(args: &Args) -> Result<()> {
+  let client = redis::Client::open(args.redis.to_string())?;
+  let mut con = client.get_multiplexed_async_connection().await?;
+  con.publish::<_, _, ()>("redis_channel", format!("published from rust at {}", Local::now())).await?;
+  Ok(())
+}
+
+async fn try_sub(args: &Args) -> Result<()> {
+  let client = redis::Client::open(args.redis.to_string())?;
+  let mut pubsub = client.get_async_pubsub().await?;
+
+  pubsub.subscribe("redis_channel").await?;
+  let mut stream = pubsub.on_message();
+
+  println!("ready lo listen to messages");
+  while let Some(msg) = stream.next().await {
+    let payload: String = msg.get_payload()?;
+    println!(">> {}", payload);
+  }
+
+  Ok(())
+}
+
 #[tokio::main]
 async fn main() -> ExitCode {
   let args = Args::parse();
@@ -38,6 +63,16 @@ async fn main() -> ExitCode {
   match args.mode.as_str() {
     "check" => try_check(&args).await,
     "test" => try_test(&args).await,
-    _ => Err(anyhow!("This `--mode` value is not supported."))
-  }.map_or_else(|err| { println!("Error: {}", err); 1 }, |_| 0).into()
+    "pub" => try_pub(&args).await,
+    "sub" => try_sub(&args).await,
+    _ => Err(anyhow!("This `--mode` value is not supported.")),
+  }
+  .map_or_else(
+    |err| {
+      println!("Error: {}", err);
+      1
+    },
+    |_| 0,
+  )
+  .into()
 }
