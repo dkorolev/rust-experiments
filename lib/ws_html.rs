236107982ac7399f739ff0636c666ecf8f7cf7df
Dima Korolev <dima@current.ai>
2025-04-22 18:25:38 -0700

    A bit cleaner.

--- lib/ws_html.rs
+++ lib/ws_html.rs
@@ -1,7 +1,7 @@
 use askama::Template;
 
 #[derive(Template)]
-#[template(path = "ws.html", escape = "none")]
+#[template(path = "ws.html")]
 pub struct WsHtmlTemplate<'a> {
   pub ws_address: &'a str,
 }
