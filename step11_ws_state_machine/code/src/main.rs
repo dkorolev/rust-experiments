b196b836203bf089786b48754b3782f321437715
Dima Korolev <dima@current.ai>
2025-04-28 19:02:18 -0700

    Proper wrapping, preparing for tests for real.

--- step11_ws_state_machine/code/src/main.rs
+++ step11_ws_state_machine/code/src/main.rs
@@ -19,6 +19,8 @@ use tokio::{
   sync::{mpsc, Mutex},
 };
 
+type LogicalTimeMs = u64;
+
 #[derive(Parser)]
 struct Args {
   #[arg(long, default_value = "3000")]
@@ -26,7 +28,7 @@ struct Args {
 }
 
 trait TimerTrait: Send + Sync {
-  fn millis_since_start(&self) -> u64;
+  fn millis_since_start(&self) -> LogicalTimeMs;
 }
 
 struct Timer {
@@ -40,45 +42,44 @@ impl Timer {
 }
 
 impl TimerTrait for Timer {
-  fn millis_since_start(&self) -> u64 {
-    self.start_time.elapsed().as_millis() as u64
+  fn millis_since_start(&self) -> LogicalTimeMs {
+    self.start_time.elapsed().as_millis() as LogicalTimeMs
   }
 }
 
-struct FiniteStateMachine {
-  next_task_id: u64,
-  pending_operations: BinaryHeap<StateMachineAdvancer>,
-  active_tasks: std::collections::HashMap<u64, String>,
+trait WriterTrait: Send + Sync {
+  fn write_text<'a>(&'a self, text: String) -> Pin<Box<dyn Future<Output = ()> + Send + 'a>>;
 }
 
-struct OutputWrapper {
+struct WebSocketWriter {
   socket: Arc<Mutex<WebSocket>>,
 }
 
-impl OutputWrapper {
-  async fn write_impl(&self, text: impl Into<axum::extract::ws::Message>) {
-    let mut socket = self.socket.lock().await;
-    let _ = socket.send(text.into()).await;
-  }
-
-  async fn write(&self, text: String) {
-    self.write_impl(text).await;
+impl Clone for WebSocketWriter {
+  fn clone(&self) -> Self {
+    Self { socket: self.socket.clone() }
   }
 }
 
-enum StepResult {
-  Completed,
-  FixedSleep(u64, TaskState),
-  ResumeInstantly(TaskState),
-  WriteAnd(String, TaskState),
+impl WebSocketWriter {
+  fn new(socket: WebSocket) -> Self {
+    Self { socket: Arc::new(Mutex::new(socket)) }
+  }
 }
 
-use crate::StepResult::*;
+impl WriterTrait for WebSocketWriter {
+  fn write_text<'a>(&'a self, text: String) -> Pin<Box<dyn Future<Output = ()> + Send + 'a>> {
+    Box::pin(async move {
+      let mut socket = self.socket.lock().await;
+      let _ = socket.send(Message::Text(text.into())).await;
+    })
+  }
+}
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 enum TaskState {
-  DelayedMessageTaskBegin(u64, String),
-  DelayedMessageTaskExecute(u64, String),
+  DelayedMessageTaskBegin(LogicalTimeMs, String),
+  DelayedMessageTaskExecute(LogicalTimeMs, String),
   DivisorsTaskBegin(u64),
   DivisorsTaskIteration(u64, u64),
   DivisorsPrintAndMoveOn(u64, u64),
@@ -88,54 +89,27 @@ enum TaskState {
 fn global_step(state: &TaskState) -> StepResult {
   match state {
     TaskState::DelayedMessageTaskBegin(sleep_ms, message) => {
-      FixedSleep(*sleep_ms, TaskState::DelayedMessageTaskExecute(*sleep_ms, message.clone()))
+      StepResult::FixedSleep(*sleep_ms, TaskState::DelayedMessageTaskExecute(*sleep_ms, message.clone()))
     }
     TaskState::DelayedMessageTaskExecute(sleep_ms, message) => {
-      WriteAnd(format!("Delayed by {}ms: `{}`.", sleep_ms, message), TaskState::Completed)
+      StepResult::WriteAnd(format!("Delayed by {}ms: `{}`.", sleep_ms, message), TaskState::Completed)
     }
-    TaskState::DivisorsTaskBegin(n) => ResumeInstantly(TaskState::DivisorsTaskIteration(*n, *n)),
+    TaskState::DivisorsTaskBegin(n) => StepResult::ResumeInstantly(TaskState::DivisorsTaskIteration(*n, *n)),
     TaskState::DivisorsTaskIteration(n, arg_i) => {
       let mut i = *arg_i;
-      while i > 0 && n % i != 0 {
+      while i > 0 && *n % i != 0 {
         i -= 1
       }
       if i == 0 {
-        WriteAnd(format!("Done for {}!", n), TaskState::Completed)
+        StepResult::WriteAnd(format!("Done for {}!", n), TaskState::Completed)
       } else {
-        FixedSleep(i * 10, TaskState::DivisorsPrintAndMoveOn(*n, i))
+        StepResult::FixedSleep(i * 10, TaskState::DivisorsPrintAndMoveOn(*n, i))
       }
     }
     TaskState::DivisorsPrintAndMoveOn(n, i) => {
-      WriteAnd(format!("A divisor of {} is {}.", n, i), TaskState::DivisorsTaskIteration(*n, i - 1))
+      StepResult::WriteAnd(format!("A divisor of {} is {}.", n, i), TaskState::DivisorsTaskIteration(*n, i - 1))
     }
-    TaskState::Completed => Completed,
-  }
-}
-struct StateMachineAdvancer {
-  scheduled_timestamp: u64,
-  state: TaskState,
-  writer: Arc<OutputWrapper>,
-  task_id: u64,
-}
-
-impl Eq for StateMachineAdvancer {}
-
-impl PartialEq for StateMachineAdvancer {
-  fn eq(&self, other: &Self) -> bool {
-    self.scheduled_timestamp == other.scheduled_timestamp
-  }
-}
-
-impl Ord for StateMachineAdvancer {
-  fn cmp(&self, other: &Self) -> Ordering {
-    // Reversed order by design.
-    other.scheduled_timestamp.cmp(&self.scheduled_timestamp)
-  }
-}
-
-impl PartialOrd for StateMachineAdvancer {
-  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-    Some(self.cmp(other))
+    TaskState::Completed => StepResult::Completed,
   }
 }
 
@@ -146,25 +120,78 @@ struct AppState {
 }
 
 impl AppState {
-  async fn schedule(&self, socket: WebSocket, state: TaskState, task_description: String) {
+  async fn schedule(
+    &self, writer: Arc<dyn WriterTrait>, state: TaskState, scheduled_timestamp: LogicalTimeMs, task_description: String,
+  ) {
     let mut fsm = self.fsm.lock().await;
     let task_id = fsm.next_task_id;
     fsm.next_task_id += 1;
 
-    fsm.active_tasks.insert(task_id, task_description);
-
-    fsm.pending_operations.push(StateMachineAdvancer {
-      scheduled_timestamp: self.timer.millis_since_start(),
-      state,
-      writer: Arc::new(OutputWrapper { socket: Arc::new(Mutex::new(socket)) }),
+    fsm.active_tasks.insert(
       task_id,
-    });
+      FiniteStateMachineTask { description: task_description, state: state, scheduled_timestamp, writer },
+    );
+
+    fsm.pending_operations.push(TaskIdWithTimestamp { scheduled_timestamp, task_id });
   }
 }
 
 #[derive(Clone)]
+struct FiniteStateMachineTask {
+  description: String,
+  state: TaskState,
+  scheduled_timestamp: LogicalTimeMs,
+  writer: Arc<dyn WriterTrait>,
+}
+
+struct TaskIdWithTimestamp {
+  scheduled_timestamp: LogicalTimeMs,
+  task_id: u64,
+}
+
+impl Eq for TaskIdWithTimestamp {}
+
+impl PartialEq for TaskIdWithTimestamp {
+  fn eq(&self, other: &Self) -> bool {
+    self.scheduled_timestamp == other.scheduled_timestamp
+  }
+}
+
+impl Ord for TaskIdWithTimestamp {
+  fn cmp(&self, other: &Self) -> Ordering {
+    // Reversed order by design.
+    other.scheduled_timestamp.cmp(&self.scheduled_timestamp)
+  }
+}
+
+impl PartialOrd for TaskIdWithTimestamp {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.cmp(other))
+  }
+}
+
+struct FiniteStateMachine {
+  next_task_id: u64,
+  pending_operations: BinaryHeap<TaskIdWithTimestamp>,
+  active_tasks: std::collections::HashMap<u64, FiniteStateMachineTask>,
+}
+
+enum StepResult {
+  Completed,
+  FixedSleep(LogicalTimeMs, TaskState),
+  ResumeInstantly(TaskState),
+  WriteAnd(String, TaskState),
+}
+
+#[derive(Clone)]
 struct AckermannContext {
-  wrapped_socket: Arc<Mutex<WebSocket>>,
+  writer: Arc<dyn WriterTrait>,
+}
+
+impl AckermannContext {
+  fn new(writer: Arc<dyn WriterTrait>) -> Self {
+    Self { writer }
+  }
 }
 
 async fn add_handler(
@@ -174,7 +201,7 @@ async fn add_handler(
 }
 
 async fn add_handler_ws(mut socket: WebSocket, a: i32, b: i32, _state: Arc<AppState>) {
-  let _ = socket.send(Message::Text((a + b).to_string().into())).await;
+  let _ = socket.send(Message::Text(format!("{}", a + b).into())).await;
 }
 
 async fn ackermann_handler(
@@ -201,15 +228,10 @@ fn async_ack(ctx: AckermannContext, m: i64, n: i64, indent: usize) -> Pin<Box<dy
     if m == 0 {
       tokio::time::sleep(std::time::Duration::from_millis(10)).await;
       let msg = format!("{}ack({m},{n}) = {}", indentation, n + 1);
-      let mut socket = ctx.wrapped_socket.lock().await;
-      let _ = socket.send(Message::Text(msg.into())).await;
+      ctx.writer.write_text(msg).await;
       n + 1
     } else {
-      {
-        let mut socket = ctx.wrapped_socket.lock().await;
-        tokio::time::sleep(std::time::Duration::from_millis(10)).await;
-        let _ = socket.send(Message::Text(format!("{}ack({m},{n}) ...", indentation).into())).await;
-      }
+      ctx.writer.write_text(format!("{}ack({m},{n}) ...", indentation)).await;
 
       let r = match (m, n) {
         (0, n) => n + 1,
@@ -218,42 +240,44 @@ fn async_ack(ctx: AckermannContext, m: i64, n: i64, indent: usize) -> Pin<Box<dy
       };
 
       tokio::time::sleep(std::time::Duration::from_millis(10)).await;
-
-      {
-        let mut socket = ctx.wrapped_socket.lock().await;
-        let _ = socket.send(Message::Text(format!("{}ack({m},{n}) = {r}", indentation).into())).await;
-      }
-
+      ctx.writer.write_text(format!("{}ack({m},{n}) = {r}", indentation)).await;
       r
     }
   })
 }
 
 async fn ackermann_handler_ws(socket: WebSocket, m: i64, n: i64, _state: Arc<AppState>) {
-  let ctx = AckermannContext { wrapped_socket: Arc::new(Mutex::new(socket)) };
+  let ctx = AckermannContext::new(Arc::new(WebSocketWriter::new(socket)));
   let _ = async_ack(ctx, m, n, 0).await;
 }
 
 async fn delay_handler(
-  ws: WebSocketUpgrade, Path((t, s)): Path<(u64, String)>, State(state): State<Arc<AppState>>,
+  ws: WebSocketUpgrade, Path((t, s)): Path<(LogicalTimeMs, String)>, State(state): State<Arc<AppState>>,
 ) -> impl IntoResponse {
-  ws.on_upgrade(move |socket| delay_handler_ws(socket, t, s, state))
+  ws.on_upgrade(move |socket| delay_handler_ws(socket, state.timer.millis_since_start(), t, s, state))
 }
 
-async fn delay_handler_ws(socket: WebSocket, t: u64, s: String, state: Arc<AppState>) {
+async fn delay_handler_ws(socket: WebSocket, ts: LogicalTimeMs, t: u64, s: String, state: Arc<AppState>) {
   state
-    .schedule(socket, TaskState::DelayedMessageTaskBegin(t, s.clone()), format!("Delayed by {}ms: `{}`.", t, s))
+    .schedule(
+      Arc::new(WebSocketWriter::new(socket)),
+      TaskState::DelayedMessageTaskBegin(t, s.clone()),
+      ts,
+      format!("Delayed by {}ms: `{}`.", t, s),
+    )
     .await;
 }
 
 async fn divisors_handler(
   ws: WebSocketUpgrade, Path(a): Path<u64>, State(state): State<Arc<AppState>>,
 ) -> impl IntoResponse {
-  ws.on_upgrade(move |socket| divisors_handler_ws(socket, a, state))
+  ws.on_upgrade(move |socket| divisors_handler_ws(socket, state.timer.millis_since_start(), a, state))
 }
 
-async fn divisors_handler_ws(socket: WebSocket, n: u64, state: Arc<AppState>) {
-  state.schedule(socket, TaskState::DivisorsTaskBegin(n), format!("Divisors of {}", n)).await;
+async fn divisors_handler_ws(socket: WebSocket, ts: LogicalTimeMs, n: u64, state: Arc<AppState>) {
+  state
+    .schedule(Arc::new(WebSocketWriter::new(socket)), TaskState::DivisorsTaskBegin(n), ts, format!("Divisors of {}", n))
+    .await;
 }
 
 async fn root_handler(_state: State<Arc<AppState>>) -> impl IntoResponse {
@@ -261,15 +285,12 @@ async fn root_handler(_state: State<Arc<AppState>>) -> impl IntoResponse {
 }
 
 async fn state_handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-  let active_tasks_copy = {
-    let fsm = state.fsm.lock().await;
-    fsm.active_tasks.clone()
-  };
+  let active_tasks_copy = state.fsm.lock().await.active_tasks.clone();
 
   let mut response = String::from("Active tasks:\n");
 
-  for (id, description) in active_tasks_copy.iter() {
-    response.push_str(&format!("Task ID: {}, Description: {}\n", id, description));
+  for (id, task) in active_tasks_copy.iter() {
+    response.push_str(&format!("Task ID: {}, Description: {}, State: {:?}\n", id, task.description, task.state));
   }
 
   if active_tasks_copy.is_empty() {
@@ -286,13 +307,22 @@ async fn quit_handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
 
 async fn execute_pending_operations(state: Arc<AppState>) {
   loop {
-    let now = state.timer.millis_since_start();
-    let task_to_execute = {
+    execute_pending_operations_inner(state.clone()).await;
+
+    // NOTE(dkorolev): I will eventually rewrite this w/o busy waiting.
+    tokio::time::sleep(Duration::from_millis(10)).await;
+  }
+}
+
+async fn execute_pending_operations_inner(state: Arc<AppState>) {
+  loop {
+    let now: LogicalTimeMs = state.timer.millis_since_start();
+    let task_id_to_execute = {
       let mut fsm = state.fsm.lock().await;
       if let Some(peek) = fsm.pending_operations.peek() {
         if peek.scheduled_timestamp <= now {
-          if let Some(state_machine_advancer) = fsm.pending_operations.pop() {
-            Some(state_machine_advancer)
+          if let Some(task_with_timestamp) = fsm.pending_operations.pop() {
+            Some(task_with_timestamp.task_id)
           } else {
             None
           }
@@ -304,36 +334,64 @@ async fn execute_pending_operations(state: Arc<AppState>) {
       }
     };
 
-    if let Some(mut state_machine_advancer) = task_to_execute {
-      let original_timestamp = state_machine_advancer.scheduled_timestamp;
-      match global_step(&state_machine_advancer.state) {
-        FixedSleep(delay_ms, new_state) => {
-          state_machine_advancer.state = new_state;
-          state_machine_advancer.scheduled_timestamp = original_timestamp + delay_ms;
-          let mut fsm = state.fsm.lock().await;
-          fsm.pending_operations.push(state_machine_advancer);
+    if let Some(task_id) = task_id_to_execute {
+      let task_info = {
+        let fsm = state.fsm.lock().await;
+        if let Some(task) = fsm.active_tasks.get(&task_id) {
+          (task.state.clone(), task.scheduled_timestamp)
+        } else {
+          continue;
         }
-        ResumeInstantly(new_state) => {
-          state_machine_advancer.state = new_state;
-          state_machine_advancer.scheduled_timestamp = original_timestamp;
+      };
+
+      let (task_state, original_timestamp) = task_info;
+
+      match global_step(&task_state) {
+        StepResult::FixedSleep(delay_ms, new_state) => {
           let mut fsm = state.fsm.lock().await;
-          fsm.pending_operations.push(state_machine_advancer);
+
+          if let Some(task) = fsm.active_tasks.get_mut(&task_id) {
+            task.state = new_state;
+            task.scheduled_timestamp = original_timestamp + delay_ms;
+          }
+
+          fsm
+            .pending_operations
+            .push(TaskIdWithTimestamp { scheduled_timestamp: original_timestamp + delay_ms, task_id });
         }
-        WriteAnd(message, next_step) => {
-          state_machine_advancer.writer.write(message).await;
-          state_machine_advancer.state = next_step;
+        StepResult::ResumeInstantly(new_state) => {
           let mut fsm = state.fsm.lock().await;
-          fsm.pending_operations.push(state_machine_advancer);
+
+          if let Some(task) = fsm.active_tasks.get_mut(&task_id) {
+            task.state = new_state;
+          }
+
+          fsm.pending_operations.push(TaskIdWithTimestamp { scheduled_timestamp: original_timestamp, task_id });
         }
-        Completed => {
+        StepResult::WriteAnd(message, next_step) => {
+          {
+            let fsm = state.fsm.lock().await;
+            if let Some(task) = fsm.active_tasks.get(&task_id) {
+              task.writer.write_text(message).await;
+            }
+          }
+
           let mut fsm = state.fsm.lock().await;
-          fsm.active_tasks.remove(&state_machine_advancer.task_id);
+
+          if let Some(task) = fsm.active_tasks.get_mut(&task_id) {
+            task.state = next_step;
+          }
+
+          fsm.pending_operations.push(TaskIdWithTimestamp { scheduled_timestamp: original_timestamp, task_id });
+        }
+        StepResult::Completed => {
+          let mut fsm = state.fsm.lock().await;
+          fsm.active_tasks.remove(&task_id);
         }
       }
+    } else if state.fsm.lock().await.pending_operations.is_empty() {
+      break;
     }
-
-    // NOTE(dkorolev): I will eventually rewrite this w/o busy waiting.
-    tokio::time::sleep(Duration::from_millis(10)).await;
   }
 }
 
@@ -346,7 +404,7 @@ async fn main() {
   let app_state = Arc::new(AppState {
     fsm: Arc::new(Mutex::new(FiniteStateMachine {
       next_task_id: 0,
-      pending_operations: BinaryHeap::<StateMachineAdvancer>::new(),
+      pending_operations: BinaryHeap::<TaskIdWithTimestamp>::new(),
       active_tasks: std::collections::HashMap::new(),
     })),
     quit_tx,
